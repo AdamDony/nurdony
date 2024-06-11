052cc95c9022ad4ea8354f8b686bdfe5410bb6ef
AdamDony <nuradamdonyeeeruet@gmail.com>
2024-06-11 21:21:24 +0000

    Completed Homework 3

--- packages/turtle_square/src/turtle_distance.py
+++ packages/turtle_square/src/turtle_distance.py
@@ -18,15 +18,30 @@ class TurtleDistanceNode:
             distance = math.sqrt((data.x - self.prev_x)**2 + (data.y - self.prev_y)**2)
             self.distance_traveled += distance
 
+            unit = rospy.get_param('/distance_unit', 'meters')
+            converted_distance, unit_label = self.convert_distance(self.distance_traveled, unit)
+
             msg = UnitsLabelled()
-            msg.value = self.distance_traveled
-            msg.units = "meters"
+            msg.value = converted_distance
+            msg.units = unit_label
             self.publisher.publish(msg)
 
         self.prev_x = data.x
         self.prev_y = data.y
 
+    def convert_distance(self, distance, unit):
+        if unit == 'meters':
+            return distance, 'meters'
+        elif unit == 'feet':
+            return distance * 3.28084, 'feet'
+        elif unit == 'smoots':
+            return distance / 1.7018, 'smoots'
+        else:
+            rospy.logwarn("Unknown unit: %s. Defaulting to meters.", unit)
+            return distance, 'meters'
+
 if __name__ == '__main__':
     rospy.init_node('turtle_distance_node')
     node = TurtleDistanceNode()
     rospy.spin()
+
