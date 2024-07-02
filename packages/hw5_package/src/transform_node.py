9cb3e4a556eb4be78c7fda44e91e95d5426e4123
AdamDony <nuradamdonyeeeruet@gmail.com>
2024-07-02 05:42:30 +0000

    Add HW-5 ROS node and launch file for coordinate transforms

--- packages/hw5_package/src/transform_node.py
+++ packages/hw5_package/src/transform_node.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python3
+
 import rospy
-import math
 from duckietown_msgs.msg import Vector2D
+import numpy as np
 
-class TransformNode:
-    def __init__(self):
-        self.sensor_to_robot = [1, 0]
-        self.robot_to_world_rotation = [[-math.sqrt(2)/2, -math.sqrt(2)/2], 
-                                        [math.sqrt(2)/2, -math.sqrt(2)/2]]
-        self.robot_to_world_translation = [5, 3]
+# Transformation matrices
+T_sensor_to_robot = np.array([
+    [1, 0, -1],
+    [0, 1, 0],
+    [0, 0, 1]
+])
 
-        rospy.Subscriber('/sensor_coord', Vector2D, self.transform_callback)
-        self.robot_coord_pub = rospy.Publisher('/robot_coord', Vector2D, queue_size=10)
-        self.world_coord_pub = rospy.Publisher('/world_coord', Vector2D, queue_size=10)
+T_robot_to_world = np.array([
+    [-np.sqrt(2)/2, -np.sqrt(2)/2, 5 + np.sqrt(2)/2],
+    [np.sqrt(2)/2, -np.sqrt(2)/2, 3 - np.sqrt(2)/2],
+    [0, 0, 1]
+])
 
-    def transform_callback(self, msg):
-        sensor_x, sensor_y = msg.x, msg.y
+T_sensor_to_world = np.dot(T_robot_to_world, T_sensor_to_robot)
 
-        # Transform to robot frame
-        robot_x = sensor_x + self.sensor_to_robot[0]
-        robot_y = sensor_y + self.sensor_to_robot[1]
-        robot_coord = Vector2D()
-        robot_coord.x, robot_coord.y = robot_x, robot_y
-        self.robot_coord_pub.publish(robot_coord)
+def transform_callback(sensor_coord):
+    point_sensor = np.array([sensor_coord.x, sensor_coord.y, 1])
 
-        # Transform to world frame
-        world_x = (self.robot_to_world_rotation[0][0] * robot_x + 
-                   self.robot_to_world_rotation[0][1] * robot_y + 
-                   self.robot_to_world_translation[0])
-        world_y = (self.robot_to_world_rotation[1][0] * robot_x + 
-                   self.robot_to_world_rotation[1][1] * robot_y + 
-                   self.robot_to_world_translation[1])
-        world_coord = Vector2D()
-        world_coord.x, world_coord.y = world_x, world_y
-        self.world_coord_pub.publish(world_coord)
+    point_robot = np.dot(T_sensor_to_robot, point_sensor)
+    point_world = np.dot(T_sensor_to_world, point_sensor)
+
+    robot_coord = Vector2D(x=point_robot[0], y=point_robot[1])
+    world_coord = Vector2D(x=point_world[0], y=point_world[1])
+
+    robot_pub.publish(robot_coord)
+    world_pub.publish(world_coord)
 
 if __name__ == '__main__':
-    rospy.init_node('transform_node', anonymous=True)
-    node = TransformNode()
+    rospy.init_node('transform_points')
+    
+    robot_pub = rospy.Publisher('/robot_coord', Vector2D, queue_size=10)
+    world_pub = rospy.Publisher('/world_coord', Vector2D, queue_size=10)
+
+    rospy.Subscriber('/sensor_coord', Vector2D, transform_callback)
+
     rospy.spin()
+
